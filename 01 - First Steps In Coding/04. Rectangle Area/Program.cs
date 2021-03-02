64cf3df6d4f4e069a8284c8471229b1fed78e563
Ivan Stamboliyski <ivanst.atis@gmail.com>
2021-03-02 14:29:08 +0200

    Add files via upload

---
+++ 01 - First Steps In Coding/04. Rectangle Area/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._1_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 125;
+            int area = a * b;
+
+            Console.WriteLine(area);
+        }
+    }
+}
